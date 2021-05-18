ec0c84079aba51fed51b4238e843a4b3b7fda440
Alex Collins <sullenumbra@gmail.com>
2021-05-18 16:16:15 -0500

    can now clear worries with settings button

--- app/src/main/java/com/agc/worrywhy/MainActivity.kt
+++ app/src/main/java/com/agc/worrywhy/MainActivity.kt
@@ -40,7 +40,10 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_settings -> {
+                viewModel.removeAllWorries()
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
