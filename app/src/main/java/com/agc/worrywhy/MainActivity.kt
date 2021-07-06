2c25431ad3041160149cad5bf87760f7dd103c49
Alex Collins <sullenumbra@gmail.com>
2021-07-06 10:09:06 -0500

    up button in action bar

--- app/src/main/java/com/agc/worrywhy/MainActivity.kt
+++ app/src/main/java/com/agc/worrywhy/MainActivity.kt
@@ -3,7 +3,9 @@ package com.agc.worrywhy
 import android.os.Bundle
 import androidx.annotation.IdRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -13,36 +15,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar_main)
-
-        navigation_bottom.setOnNavigationItemSelectedListener {
-            when (it.itemId) {
-                R.id.menu_routine -> {
-                    println("routine")
-                    false
-                }
-                R.id.menu_coping -> {
-                    println("coping")
-                    false
-                }
-                R.id.menu_worries -> {
-                    popOrGo(R.id.worryListFragment)
-                    true
-                }
-                else -> false
-            }
-        }
+        setupActionBarWithNavController(findNavController(R.id.nav_host))
     }
 
-    private fun popOrGo(@IdRes destinationId: Int) {
-        findNavController(R.id.nav_host).apply {
-            if (currentDestination?.id != destinationId && !popBackStack(
-                    destinationId,
-                    false
-                )
-            ) {
-                navigate(destinationId)
-            }
-        }
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host).navigateUp() || super.onSupportNavigateUp()
     }
 }
 
